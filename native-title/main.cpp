ca2258f584cb4678b6033bd6735043c6b6b5bf0f
robin2kk <124818770+robin2kk@users.noreply.github.com>
2026-09-22 16:00:08 -0700

    Isolate native DualSense on proven Build 001 renderer

--- native-title/main.cpp
+++ native-title/main.cpp
@@ -1,6 +1,4 @@
 #include "diagnostic_frame.h"
-#include "console_ui.h"
-#include "rom_scanner.h"
 #include "pad_ps5.h"
 #include <unistd.h>
 extern "C" int sceSystemServiceHideSplashScreen(void);
@@ -8,17 +6,16 @@ extern "C" int gg_platform_display_open(unsigned,unsigned);
 extern "C" int gg_platform_display_present(void);
 int main(){
  if(!gg_platform_display_open(1920,1080))for(;;)usleep(1000000);
- sceSystemServiceHideSplashScreen();gg_pad_open();
- const char*ids[5]={"nes","snes","n64","genesis","psx"};
- GGGameList lists[5];for(int i=0;i<5;i++)gg_scan_games(ids[i],&lists[i]);
- GGSurface s={1920,1080};int system=0,game=0;
+ sceSystemServiceHideSplashScreen();
+ GGSurface s={1920,1080};
+ int selected=2;
+ int pad_ok=gg_pad_open();
  for(;;){
-  uint32_t p=gg_pad_pressed();
-  if(p&GG_PAD_LEFT){if(game>0)game--;}
-  if(p&GG_PAD_RIGHT){if(game+1<lists[system].count)game++;}
-  if(p&GG_PAD_CIRCLE){system=(system+1)%5;game=0;}
-  gg_draw_console_browser(s,system,game);
-  if(!gg_platform_display_present())for(;;)usleep(1000000);
-  usleep(16000);
+   uint32_t p=pad_ok?gg_pad_pressed():0;
+   if((p&GG_PAD_LEFT)&&selected>0)selected--;
+   if((p&GG_PAD_RIGHT)&&selected<4)selected++;
+   gg_draw_retrohub_frame(s,selected);
+   if(!gg_platform_display_present())for(;;)usleep(1000000);
+   usleep(16000);
  }
 }
