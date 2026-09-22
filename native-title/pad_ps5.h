a6a42086b068cae30b522a52c3ac16ce1646fba2
robin2kk <124818770+robin2kk@users.noreply.github.com>
2026-09-22 15:22:18 -0700

    Add native DualSense navigation for Build 002

---
+++ native-title/pad_ps5.h
@@ -0,0 +1,14 @@
+#ifndef GOLDENGAMES_NATIVE_PAD_H
+#define GOLDENGAMES_NATIVE_PAD_H
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+enum { GG_PAD_LEFT=1u<<0,GG_PAD_RIGHT=1u<<1,GG_PAD_CROSS=1u<<2,GG_PAD_CIRCLE=1u<<3 };
+int gg_pad_open(void);
+uint32_t gg_pad_pressed(void);
+void gg_pad_close(void);
+#ifdef __cplusplus
+}
+#endif
+#endif
