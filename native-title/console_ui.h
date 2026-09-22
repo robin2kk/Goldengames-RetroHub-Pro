07725d163207f84e224a537c414c02fd513857b0
robin2kk <124818770+robin2kk@users.noreply.github.com>
2026-09-22 15:27:30 -0700

    Add distinct per-console browser identities

---
+++ native-title/console_ui.h
@@ -0,0 +1,11 @@
+#ifndef GG_CONSOLE_UI_H
+#define GG_CONSOLE_UI_H
+#include "diagnostic_frame.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+void gg_draw_console_browser(GGSurface s,int system,int selected_game);
+#ifdef __cplusplus
+}
+#endif
+#endif
