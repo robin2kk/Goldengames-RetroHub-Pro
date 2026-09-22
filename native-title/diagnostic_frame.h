36096d79d9ddf944e07a7e84ce53d04a3c97f86b
robin2kk <124818770+robin2kk@users.noreply.github.com>
2026-09-22 15:08:47 -0700

    Route GoldenGames acceptance renderer through PS5 tiled VideoOut

--- native-title/diagnostic_frame.h
+++ native-title/diagnostic_frame.h
@@ -1,6 +1,7 @@
 #ifndef GOLDENGAMES_NATIVE_TITLE_DIAGNOSTIC_H
 #define GOLDENGAMES_NATIVE_TITLE_DIAGNOSTIC_H
 #include <stdint.h>
-typedef struct { uint32_t *pixels; unsigned width; unsigned height; unsigned pitch_pixels; } GGSurface;
+typedef struct { unsigned width; unsigned height; } GGSurface;
+void gg_platform_put_pixel(unsigned x,unsigned y,uint32_t color);
 void gg_draw_acceptance_frame(GGSurface s);
 #endif
