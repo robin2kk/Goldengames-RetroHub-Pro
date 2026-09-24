08bca9d2cea6c8a2cf5b2c4e722eefb12fd0ab2e
robin2kk <124818770+robin2kk@users.noreply.github.com>
2026-09-24 13:41:43 -0700

    Increase ROM path capacity for native app0 content paths

--- native-title/rom_scanner.h
+++ native-title/rom_scanner.h
@@ -1,7 +1,7 @@
 #ifndef GG_ROM_SCANNER_H
 #define GG_ROM_SCANNER_H
 #define GG_MAX_GAMES 256
-#define GG_NAME_MAX 160
+#define GG_NAME_MAX 512
 typedef struct { char filename[GG_NAME_MAX]; char title[GG_NAME_MAX]; } GGGame;
 typedef struct { GGGame games[GG_MAX_GAMES]; int count; } GGGameList;
 #ifdef __cplusplus
