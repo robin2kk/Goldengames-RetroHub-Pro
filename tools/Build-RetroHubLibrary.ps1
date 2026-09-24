f9e92a80d66be658b3a5241630eab110448b7c86
robin2kk <124818770+robin2kk@users.noreply.github.com>
2026-09-24 15:25:24 -0700

    Expand RetroHub library generator to full console set

--- tools/Build-RetroHubLibrary.ps1
+++ tools/Build-RetroHubLibrary.ps1
@@ -1,26 +1,25 @@
 param(
-  [Parameter(Mandatory=$true)][string]$RetroArchRoot,
-  [string]$Output = ".\\RetroHub-library",
-  [string]$PS5RetroArchRoot = "/data/homebrew/RetroArch"
+ [Parameter(Mandatory=$true)][string]$RetroArchRoot,
+ [string]$Output=".\\RetroHub-library",
+ [string]$PS5RetroArchRoot="/data/homebrew/RetroArch"
 )
-$ErrorActionPreference = "Stop"
-$systems = @{
-  nes=@(".nes",".zip"); snes=@(".sfc",".smc",".zip"); n64=@(".z64",".n64",".v64");
-  genesis=@(".md",".gen",".bin",".zip"); psx=@(".cue",".chd",".pbp")
+$ErrorActionPreference="Stop"
+$systems=[ordered]@{
+ nes=@(".nes",".zip"); snes=@(".sfc",".smc",".zip"); n64=@(".z64",".n64",".v64");
+ gb=@(".gb",".zip"); gbc=@(".gbc",".zip"); gba=@(".gba",".zip");
+ genesis=@(".md",".gen",".bin",".zip"); segacd=@(".cue",".chd"); x32=@(".32x",".bin");
+ saturn=@(".cue",".chd"); psx=@(".cue",".chd",".pbp");
+ atari2600=@(".a26",".bin",".zip"); atari7800=@(".a78",".bin",".zip");
+ lynx=@(".lnx",".zip"); jaguar=@(".j64",".jag",".zip"); pce=@(".pce",".zip");
+ arcade=@(".zip"); amiga=@(".adf",".hdf",".lha"); c64=@(".d64",".t64",".crt")
 }
-New-Item -ItemType Directory -Force -Path $Output | Out-Null
+New-Item -ItemType Directory -Force -Path $Output|Out-Null
 foreach($system in $systems.Keys){
-  $dir=Join-Path $RetroArchRoot ("roms\"+$system)
-  $lines=@()
-  if(Test-Path $dir){
-    Get-ChildItem -LiteralPath $dir -File | Sort-Object Name | ForEach-Object {
-      if($systems[$system] -contains $_.Extension.ToLowerInvariant()){
-        $lines += "$PS5RetroArchRoot/roms/$system/$($_.Name)"
-      }
-    }
-  }
-  $dest=Join-Path $Output "$system.lst"
-  [System.IO.File]::WriteAllLines($dest,$lines,[System.Text.UTF8Encoding]::new($false))
-  Write-Host "$system : $($lines.Count) games -> $dest"
+ $dir=Join-Path $RetroArchRoot ("roms\"+$system);$lines=@()
+ if(Test-Path $dir){Get-ChildItem -LiteralPath $dir -File|Sort-Object Name|ForEach-Object{
+  if($systems[$system] -contains $_.Extension.ToLowerInvariant()){$lines+="$PS5RetroArchRoot/roms/$system/$($_.Name)"}
+ }}
+ [IO.File]::WriteAllLines((Join-Path $Output "$system.lst"),$lines,[Text.UTF8Encoding]::new($false))
+ Write-Host "$system : $($lines.Count)"
 }
-Write-Host "Copy the generated .lst files into PPSA99202/library/."
+Write-Host "Copy generated .lst files to PPSA99202/library/."
