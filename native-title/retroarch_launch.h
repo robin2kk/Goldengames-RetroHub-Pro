319e86ebb3cc48dc7f23a15d39ed40d1b8fbd6ed
robin2kk <124818770+robin2kk@users.noreply.github.com>
2026-09-24 13:47:06 -0700

    Add native RetroArch title launch interface

---
+++ native-title/retroarch_launch.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stddef.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+int gg_launch_retroarch(const char *title_id,const char *core_path,const char *content_path);
+#ifdef __cplusplus
+}
+#endif
