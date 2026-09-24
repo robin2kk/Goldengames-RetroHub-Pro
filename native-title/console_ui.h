9c2fe828c38867f3b8c9e15eab2485d95c19e54a
robin2kk <124818770+robin2kk@users.noreply.github.com>
2026-09-24 13:01:52 -0700

    Pass scanned ROM list into native console browser

--- native-title/console_ui.h
+++ native-title/console_ui.h
@@ -1,10 +1,11 @@
 #ifndef GG_CONSOLE_UI_H
 #define GG_CONSOLE_UI_H
 #include "diagnostic_frame.h"
+#include "rom_scanner.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
-void gg_draw_console_browser(GGSurface s,int system,int selected_game);
+void gg_draw_console_browser(GGSurface s,int system,int selected_game,const GGGameList *list);
 #ifdef __cplusplus
 }
 #endif
